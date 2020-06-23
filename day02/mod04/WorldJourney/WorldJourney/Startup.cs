7e9a57c1ada979041d752fc0a3065a003ca433b5
root <root@localhost.localdomain>
2020-06-23 09:36:08 +0300

    Exercise 2
    Configuring Routes by Using the Routing Table

--- day02/mod04/WorldJourney/WorldJourney/Startup.cs
+++ day02/mod04/WorldJourney/WorldJourney/Startup.cs
@@ -21,7 +21,20 @@ namespace WorldJourney
         public void Configure(IApplicationBuilder app)
         {
             app.UseStaticFiles();
-            app.UseMvcWithDefaultRoute();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                     name: "TravelerRoute",
+                     template: "{controller}/{action}/{name}",
+                     constraints: new { name = "[A-Za-z ]+" },
+                     defaults: new { controller = "Traveler", action = "Index", name = "Katie Bruce" });
+
+                routes.MapRoute(
+                    name: "defaultRoute",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" },
+                    constraints: new { id = "[0-9]+" });
+            });
         }
     }
 }
