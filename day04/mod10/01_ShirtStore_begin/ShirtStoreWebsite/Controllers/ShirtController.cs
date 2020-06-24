f3cfb6ba9532adbad6d08441fc4365ff0ef24955
root <root@localhost.localdomain>
2020-06-24 22:07:43 +0300

    Module 10: Testing a Controller using a Fake Repository

--- day04/mod10/01_ShirtStore_begin/ShirtStoreWebsite/Controllers/ShirtController.cs
+++ day04/mod10/01_ShirtStore_begin/ShirtStoreWebsite/Controllers/ShirtController.cs
@@ -4,24 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ShirtStoreWebsite.Models;
+using ShirtStoreWebsite.Services;
 
 namespace ShirtStoreWebsite.Controllers
 {
     public class ShirtController : Controller
     {
+        private IShirtRepository _repository;
+
+        public ShirtController(IShirtRepository repository)
+        {
+            _repository = repository;
+        }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Shirt> shirts = _repository.GetShirts();
+            return View(shirts);
         }
 
         public IActionResult AddShirt(Shirt shirt)
         {
+            _repository.AddShirt(shirt);
             return RedirectToAction("Index");
         }
 
         public IActionResult Delete(int id)
         {
+            _repository.RemoveShirt(id);
             return RedirectToAction("Index");
         }
     }
